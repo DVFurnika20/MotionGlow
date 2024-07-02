20a0036e35976dfbba720922e8630b376eb1ef52
DVFurnika20 <DVFurnika20@codingburgas.bg>
2024-07-02 22:14:02 +0300

    Fix footer issue

--- MotionGlow/MotionGlow.PL/obj/Debug/net8.0/MotionGlow.PL.AssemblyInfo.cs
+++ MotionGlow/MotionGlow.PL/obj/Debug/net8.0/MotionGlow.PL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MotionGlow.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39365f5d330c9c5eea206481f609dc74b47c5d75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46457f303efc2050606c4f9444a4321ffca58fd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MotionGlow.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MotionGlow.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
